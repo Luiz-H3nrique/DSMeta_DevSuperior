8ec0ad181b051cb7e38e62240731e9d61c4d69b1
Luiz Henrique <luizsantos894@gmail.com>
2022-07-11 20:32:56 -0300

    Project clean

---
+++ Backend/src/test/java/com/devsperior/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsperior.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Frontend/src/App.tsx
@@ -0,0 +1,6 @@
+
+function App() {
+ return(<h1>ola mundo</h1>)
+}
+
+export default App
